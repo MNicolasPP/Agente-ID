2e5b080334a2b4399a8912a805d2b59f4653d13c
Manuel Nicolas Pacheco Peralta <a01255150@tec.mx>
2025-09-10 16:44:04 -0700

    Delete funciones directory

--- funciones/__init__.py
+++
@@ -1,5 +0,0 @@
-from .curp import extraer_curp
-from .nombres import extract_nombres_desde_path
-from .direccion import extract_direccion_desde_path
-
-__all__ = ["extract_curp_line_below", "extract_nombres_desde_path", "extract_direccion_desde_path"]
